e048b32c1c49bbf225e57f5bfcbccbc496685865
aelizalde053 <aelizalde053@gmail.com>
2024-11-13 20:56:04 -0600

    Cambio 1 Elizalde
    Confirmación Ok Elizalde

---
+++ proyectoelizalde/src/proyectoelizalde/Proyectoelizalde.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoelizalde;
+
+/**
+ *
+ * @author Angel
+ */
+public class Proyectoelizalde {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba Repositorio 1 IS Elizalde");
+        System.out.println("Base de desarrollo repositorio en la nube");
+    }
+    
+}
